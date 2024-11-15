070e0c1b52c3aae26bad91dbad4c67941f0d6f58
Akshat-singh90056 <vs22022005@gmail.com>
2024-11-15 17:05:39 +0530

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,5 +1,8 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install gcc make python3 python3-pip openjdk-17-jdk
+sudo apt install gcc make python3 python3-pip openjdk-17-jdk wget git
 sudo snap install code --classic
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install
 
